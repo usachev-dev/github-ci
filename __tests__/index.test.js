c52a677bbeb7d102f34127334bce0d36a01f7977
usachev-dev <usachev.dev@gmail.com>
2021-10-12 09:45:23 +0300

    write tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,16 @@
 const { slice } = require('../src/index.js');
 
 // BEGIN
-
+test("slice", () => {
+   expect(slice([0, 1, 2, 3])).toEqual([0, 1, 2, 3]);
+   expect(slice([0, 1, 2, 3], 0)).toEqual([0, 1, 2, 3]);
+   expect(slice([0, 1, 2, 3], 1)).toEqual([1, 2, 3]);
+   expect(slice([0, 1, 2, 3], 1, 3)).toEqual([1, 2]);
+   expect(slice([0, 1, 2, 3], 0, 99)).toEqual([0, 1, 2, 3]);
+   expect(slice([0, 1, 2, 3], -2)).toEqual([2, 3]);
+   expect(slice([0, 1, 2, 3], 0, -2)).toEqual([0, 1]);
+   expect(slice([])).toEqual([]);
+   expect(slice([], 1, 99)).toEqual([]);
+   expect(slice([1], -99)).toEqual([1]);
+});
 // END
