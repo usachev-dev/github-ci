fb1730f5000504476751a32014572090f4ea2748
usachev-dev <usachev.dev@gmail.com>
2021-10-12 09:54:31 +0300

    test badge

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,5 +12,6 @@ test("slice", () => {
    expect(slice([])).toEqual([]);
    expect(slice([], 1, 99)).toEqual([]);
    expect(slice([1], -99)).toEqual([1]);
+   expect(slice([1], -99)).toEqual([]);
 });
 // END
